ef951d664b316669b3a1b865e5f9686b3d58b394
Shay Ben-Sasson <shaybensasson@gmail.com>
2019-09-18 15:19:24 +0300

    MultiStagePythonWithPip
    Added an example of a docker with pip requirements.txt install
    that support pip caching and mutli stage build pf docker

--- PythonApp/my_script.py
+++ PythonApp/my_script.py
@@ -1,6 +1,6 @@
-import sys
-import numpy as np
 
-print('Python version: {}'.format(sys.version))
+import numpy as np
+import sys
 print('')
 print('Just a random number: {}'.format(np.random.rand()))
+print('Python version: {}'.format(sys.version))
